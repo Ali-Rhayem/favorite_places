9db64205249f1b1e2c3a10dbb80209bf24d87139
Ali Rhayem <alirhayem.1212@gmail.com>
2024-12-28 19:27:45 +0200

    Add PlacesScreen and PlacesList components for displaying user places

---
+++ lib/widgets/places_list.dart
@@ -0,0 +1,36 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Text(
+          'No places yet. Add some!',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      );
+    }
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (context, index) {
+        final place = places[index];
+        return ListTile(
+          title: Text(
+            place.title,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          onTap: () {},
+        );
+      },
+    );
+  }
+}
