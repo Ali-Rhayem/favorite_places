153e45f0bb977b206277cd3f5188a597bb9eef61
Ali Rhayem <alirhayem.1212@gmail.com>
2024-12-29 18:23:09 +0200

    Add location image display and address subtitle to PlaceDetailScreen and PlacesList

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -33,8 +33,17 @@ class PlacesList extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                 ),
           ),
+          subtitle: Text(
+            places[index].location.address,
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
           onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(builder:(ctx)=>PlaceDetailScreen(place: place) ),);
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                  builder: (ctx) => PlaceDetailScreen(place: place)),
+            );
           },
         );
       },
